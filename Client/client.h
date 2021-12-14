46a9fe317484f20cd2640d9d6d76346539dd4383
Kristian Mazzucca <mazzuccakristian@gmail.com>
2021-12-14 21:52:57 +0100

    "ShowMainMenu" & "GetUserChoice" functions added.

--- Client/client.h
+++ Client/client.h
@@ -2,9 +2,16 @@
 #include <iostream>
 #include <netinet/in.h>
 #include <arpa/inet.h>
+#include <string.h>
 
 #define PORT 8080
+#define SERVER_IP "127.0.0.1"
 
 using std::perror;
 using std::cout;
 using std::endl;
+using std::cin;
+
+void ShowMainMenu();
+
+int GetUserChoice();
