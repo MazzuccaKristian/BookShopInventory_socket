1109af2a573fe0ed50ee5aefd2aebac3a1ae8ae6
Kristian Mazzucca <mazzuccakristian@gmail.com>
2021-12-14 21:58:10 +0100

    File setup added. Prototype.

--- Server/server.h
+++ Server/server.h
@@ -1,10 +1,20 @@
 #include <sys/socket.h>
 #include <iostream>
 #include <netinet/in.h>
+#include <unistd.h>
+
+#include <iostream>
+#include <fstream> 
 
 using std::perror;
 using std::cout;
 using std::endl;
+using std::ofstream;
+using std::ifstream;
 
 #define PORT 8080
 #define CONNECTIONS 1
+#define BOOKS "books.txt"
+
+void Setup();
+
