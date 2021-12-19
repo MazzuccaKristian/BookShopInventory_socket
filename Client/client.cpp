c391c3d8ca26973cb9c6a440a5f6f2987f7170b0
Kristian Mazzucca <mazzuccakristian@gmail.com>
2021-12-19 18:15:16 +0100

    "ServerConnection" is now a function.

--- Client/client.cpp
+++ Client/client.cpp
@@ -1,101 +1,99 @@
 #include "client.h"
 
-int main(int argc, char const *argv[]){
+int main(int argc, char const *argv[])
+{
+    /* Parameters for connection */
+    int socket;
+
+    int serverSocket;
+
     // Connection params
-        int server = 0;
-        struct sockaddr_in serverAddress;
+    // struct sockaddr_in serverAddress;
     //
     // Application params
-        string data1, data2;
-        string query;
-        int queryResult;
+    string data1, data2;
+    string query;
+    int queryResult;
 
-        bool isStillWorking = true;
-        int choice;
-        char buffer[2048] = {0};
-        int dataFromServer;
-        string clientOption;
-        string username, password, user;
-        string title;
+    bool isStillWorking = true;
+    int choice;
+    char buffer[2048] = {0};
+    int dataFromServer;
+    string clientOption;
+    string username, password, user;
+    string title;
     //
 
-    // Connection
-        // Socket creation: IPv4, UDP and IP protocol.
-        if((server = socket(AF_INET, SOCK_STREAM, 0)) < 0){
-            perror("Socket creation");
-            exit(EXIT_FAILURE);
+    do
+    {
+        socket = ServerConnection();
+        if(socket < 0){
+            perror("Socket ERROR");
         }
 
-        serverAddress.sin_family = AF_INET;
-        serverAddress.sin_port = htons(PORT);
+        ShowMainMenu();
+        choice = GetUserChoice();
+        switch (choice)
+        {
+        case 0:
+            // Quit program.
+            cout << "Program ended. Please, wait..." << endl;
+            isStillWorking = false;
+            shutdown(socket, 2);
+            break;
 
-        int result = inet_pton(AF_INET, SERVER_IP, &serverAddress.sin_addr);
-        if(result == 0){
-            perror("Invalid buffer");
-            exit(EXIT_FAILURE);
-        }else if(result < 0){
-            perror("AF error");
-            exit(EXIT_FAILURE);
-        }
-
-        if(connect(server, (struct sockaddr *)&serverAddress, sizeof(serverAddress)) < 0){
-            perror("Connection");
-            exit(EXIT_FAILURE);
-        }
-    //
-    // Client side application
-        do{
-            ShowMainMenu();
-            choice = GetUserChoice();
-            switch(choice){
-                case 0:
-                    // Quit program.
-                    cout << "Program ended. Please, wait..." << endl;
-                    isStillWorking = false;
-                    break;
-
-                case 1:
-                    clientOption = "1";
-                    cout << "Enter title: ";
-                    data1 = GetData(true);
-                    query = clientOption + "|" + data1;
-                    send(server, query.c_str(), sizeof(query), 0);
-                    recv(server, buffer, sizeof(query), 0);
-                    if(buffer[0] == '1'){
-                        cout << "This book is in archive." << endl;
-                    }else{
-                        cout << "This book isn't in archive." << endl;
-                    }
-                    break;
-
-                case 2:
-                    clientOption = "2";
-                    cout << "Enter title: ";
-                    data1 = GetData(true);
-                    query = clientOption + "|" + data1;
-                    send(server, query.c_str(), sizeof(query), 0);
-                    recv(server, buffer, sizeof(query), 0);
-                    if(buffer[0] == '1'){
-                        cout << "Book rented." << endl;
-                    }else{
-                        cout << "Can't rent book." << endl;
-                    }
-                    break;
-
-                case 3:
-                    clientOption = "3";
-                    cout << "Enter title: ";
-                    data1 = GetData(true);
-                    query = clientOption + "|" + data1;
-                    send(server, query.c_str(), sizeof(query), 0);
-                    recv(server, buffer, sizeof(query), 0);
-                    if(buffer[0] == '1'){
-                        cout << "Book returned." << endl;
-                    }else{
-                        cout << "Book not returned." << endl;
-                    }
+        case 1:
+            clientOption = "1";
+            cout << "Enter title: ";
+            data1 = GetData(true);
+            query = clientOption + "|" + data1;
+            send(socket, query.c_str(), sizeof(query), 0);
+            recv(socket, buffer, sizeof(query), 0);
+            if (buffer[0] == '1')
+            {
+                cout << "This book is in archive." << endl;
             }
-        }while(isStillWorking);
+            else
+            {
+                cout << "This book isn't in archive." << endl;
+            }
+            break;
+
+        case 2:
+            clientOption = "2";
+            cout << "Enter title: ";
+            data1 = GetData(true);
+            query = clientOption + "|" + data1;
+            send(socket, query.c_str(), sizeof(query), 0);
+            recv(socket, buffer, sizeof(query), 0);
+            if (buffer[0] == '1')
+            {
+                cout << "Book rented." << endl;
+            }
+            else
+            {
+                cout << "Can't rent book." << endl;
+            }
+            break;
+
+        case 3:
+            clientOption = "3";
+            cout << "Enter title: ";
+            data1 = GetData(true);
+            query = clientOption + "|" + data1;
+            send(socket, query.c_str(), sizeof(query), 0);
+            recv(socket, buffer, sizeof(query), 0);
+            if (buffer[0] == '1')
+            {
+                cout << "Book returned." << endl;
+            }
+            else
+            {
+                cout << "Book not returned." << endl;
+            }
+            break;
+        }
+    } while (isStillWorking);
     //
     return 0;
 }
@@ -104,7 +102,8 @@ int main(int argc, char const *argv[]){
  * @brief Show main menu (client-side)
  * 
  */
-void ShowMainMenu(){
+void ShowMainMenu()
+{
     cout << "MAIN MENU" << endl;
     cout << "1. Search book;" << endl;
     cout << "2. Rent a book;" << endl;
@@ -117,18 +116,23 @@ void ShowMainMenu(){
  * 
  * @return int Validated input.
  */
-int GetUserChoice(){
+int GetUserChoice()
+{
     int input;
     bool isValid = false;
-    do{
+    do
+    {
         cout << "Enter your choice: ";
         cin >> input;
-        if(input >= 0 && input <= 5){
+        if (input >= 0 && input <= 5)
+        {
             isValid = true;
-        }else{
+        }
+        else
+        {
             cout << "Try again...." << endl;
         }
-    }while(!isValid);
+    } while (!isValid);
     return input;
 }
 
@@ -138,12 +142,45 @@ int GetUserChoice(){
  * @param dumpFlag TRUE if the buffer must be dumped.
  * @return string - Final string.
  */
-string GetData(bool dumpFlag){
+string GetData(bool dumpFlag)
+{
     string dump;
     string data;
-    if(dumpFlag){
+    if (dumpFlag)
+    {
         getline(cin, dump);
     }
     getline(cin, data);
     return data;
+}
+
+/**
+ * @brief Establish connection with server
+ * 
+ * @return int Connection socket for client
+ */
+int ServerConnection()
+{
+    int connectionSocket;
+    struct sockaddr_in serverAddress;
+    if ((connectionSocket = socket(AF_INET, SOCK_STREAM, 0)) < 0)
+    {
+        perror("Socket creation");
+        exit(EXIT_FAILURE);
+    }
+    serverAddress.sin_family = AF_INET;
+    serverAddress.sin_addr.s_addr = INADDR_ANY;
+    serverAddress.sin_port = htons(PORT);
+    int result = inet_pton(AF_INET, SERVER_IP, &serverAddress.sin_addr);
+    if (result <= 0)
+    {
+        perror("Buffer");
+        exit(EXIT_FAILURE);
+    }
+    if (connect(connectionSocket, (struct sockaddr *)&serverAddress, sizeof(serverAddress)) < 0)
+    {
+        perror("Connection");
+        exit(EXIT_FAILURE);
+    }
+    return connectionSocket;
 }
